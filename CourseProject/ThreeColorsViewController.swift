734ce0e5213ba33d2ed169fca58abd9cffd79966
AMshev <59746592+AMshev@users.noreply.github.com>
2020-04-29 19:44:34 +0300

    add transition to detail color screen from three main colors screen + code clean

--- CourseProject/ThreeColorsViewController.swift
+++ CourseProject/ThreeColorsViewController.swift
@@ -17,9 +17,15 @@ class ThreeColorsViewController: UIViewController {
     @IBOutlet weak var mainColorNameLabel: UILabel!
     @IBOutlet weak var secondaryColorName: UILabel!
     @IBOutlet weak var additionalColorName: UILabel!
+    @IBOutlet weak var colorsStack: UIStackView!
     
-    var allColors: [ColorList] = []
+    private var allColors: [ColorList] = []
+    private var recognizedColor: [ColorModel] = []
+    private var colorNumber = 0
     var sourceImage: UIImage?
+    let mainTapGesture = UITapGestureRecognizer()
+    let secondaryTapGesture = UITapGestureRecognizer()
+    let additionalTapGesture = UITapGestureRecognizer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,25 +47,10 @@ class ThreeColorsViewController: UIViewController {
                     json.result.colors.image_colors.forEach({gForRGB.append($0.g)})
                     json.result.colors.image_colors.forEach({bForRGB.append($0.b)})
                     DispatchQueue.main.sync {
-                        self.mainColor.backgroundColor = UIColor(red: rForRGB[0],
-                                                                 green: gForRGB[0],
-                                                                 blue: bForRGB[0],
-                                                                 alpha: 1)
-                        self.secondaryColor.backgroundColor = UIColor(red: rForRGB[1],
-                                                                      green: gForRGB[1],
-                                                                      blue: bForRGB[1],
-                                                                      alpha: 1)
-                        self.additionalColor.backgroundColor = UIColor(red: rForRGB[2],
-                                                                       green: gForRGB[2],
-                                                                       blue: bForRGB[2],
-                                                                       alpha: 1)
-                        self.mainColor.setNeedsDisplay()
-                        self.secondaryColor.setNeedsDisplay()
-                        self.additionalColor.setNeedsDisplay()
-                        
-                        self.mainColorNameLabel.text = self.searchColorName(rForRGB[0], gForRGB[0], bForRGB[0])
-//                        self.secondaryColorName.text = self.searchColorName(rForRGB[1], gForRGB[1], bForRGB[1])
-//                        self.additionalColorName.text = self.searchColorName(rForRGB[2], gForRGB[2], bForRGB[2])
+                        self.setColor(self.mainColor, self.mainColorNameLabel, self.mainTapGesture, rForRGB[0], gForRGB[0], bForRGB[0])
+                        self.setColor(self.secondaryColor, self.secondaryColorName, self.secondaryTapGesture, rForRGB[1], gForRGB[1], bForRGB[1])
+                        self.setColor(self.additionalColor, self.additionalColorName, self.additionalTapGesture, rForRGB[2], gForRGB[2], bForRGB[2])
+                        self.colorsStack.arrangedSubviews.forEach({$0.isUserInteractionEnabled = true})
                     }
                 } catch {
                     print("JSON error")
@@ -68,15 +59,63 @@ class ThreeColorsViewController: UIViewController {
         })
     }
     
+    private func setColor(_ colorView: UIView,
+                          _ colorNameLabel: UILabel,
+                          _ tapGesture: UITapGestureRecognizer,
+                          _ rParameter: Int,
+                          _ gParameter: Int,
+                          _ bParameter: Int) {
+        colorView.backgroundColor = UIColor(red: rParameter, green: gParameter, blue: bParameter, alpha: 1)
+        colorView.setNeedsDisplay()
+        colorNameLabel.text = searchColorName(rParameter, gParameter, bParameter)
+        tapGesture.addTarget(self, action: #selector(colorTapped(sender:)))
+        colorView.addGestureRecognizer(tapGesture)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.navigationBar.tintColor = UIColor(named: "Color")
+    }
+    
+    @objc func colorTapped(sender: UITapGestureRecognizer) {
+        guard let view = sender.view else { return }
+        switch view {
+        case mainColor:
+            colorNumber = 0
+        case secondaryColor:
+            colorNumber = 1
+        case additionalColor:
+            colorNumber = 2
+        default:
+            colorNumber = 0
+        }
+        self.performSegue(withIdentifier: "detailScreen", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailScreen" {
+            let destinationVC = segue.destination as? DetailColorViewController
+            destinationVC?.color = recognizedColor[colorNumber]
+        }
+    }
+    
     private func searchColorName(_ rColor: Int, _ gColor: Int, _ bColor: Int) -> String? {
         var colorFromList: [ColorList] = []
         var error: Int = 0
         while colorFromList.isEmpty {
-        colorFromList = allColors.filter({(
-            (rColor - error)...(rColor + error)).contains($0.rgb.r) &&
-            ((gColor - error)...(gColor + error)).contains($0.rgb.g) &&
-            ((bColor - error)...(bColor + error)).contains($0.rgb.b)})
+            colorFromList = allColors.filter({(
+                (rColor - error)...(rColor + error)).contains($0.rgb.r) &&
+                ((gColor - error)...(gColor + error)).contains($0.rgb.g) &&
+                ((bColor - error)...(bColor + error)).contains($0.rgb.b)})
             error += 1
+        }
+        if colorFromList.first != nil {
+            let model = ColorModel(name: colorFromList.first!.name,
+                                   r: colorFromList.first!.rgb.r,
+                                   g: colorFromList.first!.rgb.g,
+                                   b: colorFromList.first!.rgb.b,
+                                   hex: colorFromList.first!.hex)
+            recognizedColor.append(model)
         }
         return colorFromList.first?.name
     }
