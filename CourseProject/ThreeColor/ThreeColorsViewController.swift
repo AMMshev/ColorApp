17d60488d874e335170069ecf2d02dd6a33f9018
AMshev <59746592+AMshev@users.noreply.github.com>
2020-05-05 01:01:31 +0300

    add shadow to source image + change the way of adding tap gestures to views on three main colors viewController

--- CourseProject/ThreeColor/ThreeColorsViewController.swift
+++ CourseProject/ThreeColor/ThreeColorsViewController.swift
@@ -15,6 +15,7 @@ class ThreeColorsViewController: UIViewController {
     @IBOutlet weak var secondaryColor: UIView!
     @IBOutlet weak var additionalColor: UIView!
     @IBOutlet weak var gradientColorsView: UIView!
+    @IBOutlet weak var sourceImageBackView: UIView!
     @IBOutlet weak var mainColorNameLabel: UILabel!
     @IBOutlet weak var secondaryColorNameLabel: UILabel!
     @IBOutlet weak var additionalColorNameLabel: UILabel!
@@ -23,12 +24,6 @@ class ThreeColorsViewController: UIViewController {
     
     private var choosenColor: ColorModel = ColorModel(name: "", r: 0, g: 0, b: 0, hex: "")
     private var gradientColors: [CGColor] = []
-    private var colorNumber = 0
-    private let mainTapGesture = UITapGestureRecognizer()
-    private let secondaryTapGesture = UITapGestureRecognizer()
-    private let additionalTapGesture = UITapGestureRecognizer()
-    private let gradientTapGesture = UITapGestureRecognizer()
-    
     var sourceImage: UIImage?
     
     override func viewDidLoad() {
@@ -39,7 +34,6 @@ class ThreeColorsViewController: UIViewController {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.tintColor = UIColor(named: "navBarColor")
     }
-    
     // MARK: - segue methods
     @objc private func colorTapped(sender: UITapGestureRecognizer) {
         guard let colorParameters = sender.view?.backgroundColor?.cgColor.components else { return }
@@ -65,11 +59,14 @@ class ThreeColorsViewController: UIViewController {
         }
     }
 }
-
 // MARK: - visual methods
 extension ThreeColorsViewController {
     private func setViews() {
         navigationController?.navigationBar.isHidden = false
+        sourceImageBackView.backgroundColor = UIColor(named: "Color")
+        sourceImageBackView.layer.shadowColor = UIColor(named: "navBarColor")?.cgColor
+        sourceImageBackView.layer.shadowOffset = CGSize(width: 0.3, height: 0.5)
+        sourceImageBackView.layer.shadowOpacity = 0.5
         guard let sourceImage = sourceImage else { return }
         sourceImageView.image = sourceImage
         Networking.shared.uploadData(image: sourceImage, completion: { imageLink in
@@ -84,48 +81,37 @@ extension ThreeColorsViewController {
                         if RGBColors.count >= 1 {
                             self.setColor(colorView: self.mainColor,
                                           colorNameLabel: self.mainColorNameLabel,
-                                          tapGesture: self.mainTapGesture,
                                           rParameter: RGBColors[0].r,
                                           gParameter: RGBColors[0].g,
-                                          bParameter: RGBColors[0].b)
-                        }
+                                          bParameter: RGBColors[0].b) }
                         if RGBColors.count >= 2 {
                             self.setColor(colorView: self.secondaryColor,
                                           colorNameLabel: self.secondaryColorNameLabel,
-                                          tapGesture: self.secondaryTapGesture,
                                           rParameter: RGBColors[1].r,
                                           gParameter: RGBColors[1].g,
-                                          bParameter: RGBColors[1].b)
-                        }
+                                          bParameter: RGBColors[1].b) }
                         if RGBColors.count >= 3 {
                             self.setColor(colorView: self.additionalColor,
                                           colorNameLabel: self.additionalColorNameLabel,
-                                          tapGesture: self.additionalTapGesture,
                                           rParameter: RGBColors[2].r,
                                           gParameter: RGBColors[2].g,
-                                          bParameter: RGBColors[2].b)
-                        }
+                                          bParameter: RGBColors[2].b) }
                         self.setColor(colorView: self.gradientColorsView,
                                       colorNameLabel: self.gradientColorsLabel,
-                                      tapGesture: self.gradientTapGesture,
-                                      gradientColors: self.gradientColors)
-                        self.colorsStack.arrangedSubviews.forEach({$0.isUserInteractionEnabled = true})
-                    }
-                } catch {
-                    print("JSON error")
-                }
+                                      gradientColors: self.gradientColors) }
+                } catch { print("JSON error") }
             })
         })
     }
     private func setColor(colorView: UIView,
                           colorNameLabel: UILabel,
-                          tapGesture: UITapGestureRecognizer,
                           gradientColors: [CGColor]? = nil,
                           rParameter: Int? = nil,
                           gParameter: Int? = nil,
                           bParameter: Int? = nil) {
         colorView.isHidden = false
         colorNameLabel.isHidden = false
+        let tapGesture = UITapGestureRecognizer()
         if let rParameter = rParameter,
             let gParameter = gParameter,
             let bParameter = bParameter {
@@ -162,5 +148,4 @@ extension ThreeColorsViewController {
         })
         return gradientColors
     }
-    
 }
